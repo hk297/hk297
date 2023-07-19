ce25d6b1ec296e4f6c2444bb84fadea30ddeb3ab
hk297 <95421603+hk297@users.noreply.github.com>
2023-07-19 14:08:06 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x54f92f7ffef8ccbf7dc0baf42f9a346c6a9dc26c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
